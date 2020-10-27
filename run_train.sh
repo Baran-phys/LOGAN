0aa715e3d2184a801351ccd195f56e94e0b56c81
Hosein Hashemi <hosein.hashemi@protonmail.ch>
2020-10-27 08:38:54 +0100

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -2,10 +2,10 @@
 python train.py \
 --num_epochs 32 --num_workers 2 --batch_size 16 \
 --num_G_accumulations 2 --num_D_accumulations 2 \
---num_D_steps 1 --G_lr 2.5e-5 --D_lr 2.5e-5 --D_B2 0.999 --G_B2 0.999 \
+--num_D_steps 2 --G_lr 2.5e-5 --D_lr 2.5e-5 --D_B2 0.999 --G_B2 0.999 \
 --G_ch 32 --D_ch 32 \
 --G_attn 32 --D_attn 32 \
---G_nl inplace_Lrelu --D_nl inplace_relu \
+--G_nl inplace_Lrelu --D_nl inplace_Lrelu \
 --G_shared \
 --hier --dim_z 256 --shared_dim 128 \
 --SN_eps 1e-8 --BN_eps 1e-5 --adam_eps 1e-8 \
